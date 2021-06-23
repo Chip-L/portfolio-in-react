4865e2b34f4d30440555ef5c242f880bf98a91ec
Chip Long <chip_long@yahoo.com>
2021-06-23 17:57:24 -0600

    Moved base styles (and variables) up to public directory. Sky image still isn't working for some reason, but will figure out later

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer/PortfolioContainer";
 
-import "./app.css";
-
 const App = () => (
   <div className="variables">
     <PortfolioContainer />
