8b5d624e55331fd6b33d279e462805d85c7a1f0b
Chip Long <chip_long@yahoo.com>
2021-06-22 23:19:13 -0600

    removed unnecessary commenting from Portfolio Container

--- src/components/PortfolioContainer/PortfolioContainer.js
+++ src/components/PortfolioContainer/PortfolioContainer.js
@@ -9,7 +9,6 @@ import Contact from "./pages/Contact";
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState("Home");
   /*
-  // TODO: Add a comment describing the functionality of this method
   // This is a function that will return the page to be rendered
   const renderPage = () => {
     if (currentPage === "Home") {
@@ -29,10 +28,8 @@ export default function PortfolioContainer() {
 
   return (
     <div>
-      {/* // TODO: Add a comment describing what we are passing as props */}
       {/* This passes in the current page to the nav tab and the function to handlePageChange change via props */}
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-      {/* // TODO: Add a comment explaining what is happening on the following line */}
       {/* calls the render page function to determine which page should be displayed. */}
       {/* {renderPage()} */}
     </div>
