06842b743dc3bbf13267a36bc620194744792a24
Chip Long <chip_long@yahoo.com>
2021-06-23 20:07:58 -0600

    tweaked app.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer/PortfolioContainer";
 
 const App = () => (
-  <div className="variables">
+  <>
     <PortfolioContainer />
-  </div>
+  </>
 );
 
 export default App;
