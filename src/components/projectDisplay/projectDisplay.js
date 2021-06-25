1e33192c2f1fec12290ee3d9a909f381019c8c3e
Chip Long <chip_long@yahoo.com>
2021-06-25 00:36:10 -0600

    Portfolio page is formatted

--- src/components/projectDisplay/projectDisplay.js
+++ src/components/projectDisplay/projectDisplay.js
@@ -2,30 +2,36 @@ import React from "react";
 
 import "./ProjectDisplay.css";
 
-// https://devdesigner.xyz/dynamic-image-import-with-create-react-app/read
-// import context for images
-// const requestImageFile = require.context("../../assets/images", true);
-
 function ProjectDisplay({ demoItem }) {
   const imageURL = `/assets/images/${demoItem.imageLocation}`;
-  // console.log(demoItem);
-  // console.log(
-  //   "requestImageFile",
-  //   requestImageFile(`./${demoItem.imageLocation}`)
-  // );
-  // console.log(requestImageFile.keys());
+
   return (
     <div className="imgContainer">
-      <a href={demoItem.siteLink} target="_blank" rel="noopener noreferrer">
-        <img src={process.env.PUBLIC_URL + imageURL} alt={demoItem.altText} />
-        <div className="img-text">
+      {/* <a href={demoItem.siteLink} target="_blank" rel="noopener noreferrer"> */}
+      <img src={process.env.PUBLIC_URL + imageURL} alt={demoItem.altText} />
+      <div className="img-text">
+        <p>
           <span className="project-name">{demoItem.appName}</span>
           <br />
           <span className="technologies">
             {demoItem.technologiesUsed.join(", ")}
           </span>
-        </div>
-      </a>
+        </p>
+        <p>
+          <a href={demoItem.siteLink} target="_blank" rel="noopener noreferrer">
+            See it Live!
+          </a>
+          <br />
+          <a
+            href={demoItem.githubLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            See the Github repo!
+          </a>
+        </p>
+      </div>
+      {/* </a> */}
     </div>
   );
 }
