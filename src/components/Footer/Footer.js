1971e8e9791c29c2eafec190f390fda2e67fd998
Chip Long <chip_long@yahoo.com>
2021-06-24 23:17:12 -0600

    tweaked header formatting. working on page height

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,39 +1,34 @@
 import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
     <footer>
-      <ul class="container">
+      <ul className="container">
         <li>
-          <label for="phone">Phone:</label>
+          <label htmlFor="phone">Phone:</label>
           <a id="phone" href="tel:7202894751">
             720-289-4751
           </a>
         </li>
         <li>
-          <label for="email">Email:</label>
-          <a id="email" href="#">
-            chip_long@yahoo.com
-          </a>
-        </li>
-        <li>
-          <label for="github"> Github:</label>
+          <label htmlFor="github"> Github:</label>
           <a
             id="github"
             href="https://github.com/Chip-L/"
             target="_blank"
-            rel="noopener"
+            rel="noopener noreferrer"
           >
             Chip-L
           </a>
         </li>
         <li>
-          <label for="linkedIn">Linked In:</label>
+          <label htmlFor="linkedIn">Linked In:</label>
           <a
             id="linkedIn"
             href="https://www.linkedin.com/in/chiplong1/"
             target="_blank"
-            rel="noopener"
+            rel="noopener noreferrer"
           >
             chiplong1
           </a>
